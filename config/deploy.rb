da61dc07a58192082529459d4336d4130b0ade2d
Mathieu Rosser <mathieu.rosser@he-arc.ch>
2015-03-12 14:37:10 +0000

    Capistrano le retour

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'SocialEvents'
+set :repo_url, 'https://github.com/HE-Arc/SocialEvents.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/rails/SocialEvents'
 
 # Default value for :scm is :git
 # set :scm, :git
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-
+server 'srvz-isic04.he-arc.ch', user: 'rails', roles: %w{web app db}
 
 
 # role-based syntax
@@ -41,11 +41,12 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
+  set :ssh_options, {
+    port: 1422
 #    keys: %w(/home/rlisowski/.ssh/id_rsa),
 #    forward_agent: false,
 #    auth_methods: %w(password)
-#  }
+  }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
