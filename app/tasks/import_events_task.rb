396b4d459252efb10e19ebc05f61a00c9ae9561f
Mathieu Rosser <mathieu.rosser@he-arc.ch>
2015-03-26 13:28:53 +0000

    Cleanup import task

--- app/tasks/import_events_task.rb
+++ app/tasks/import_events_task.rb
@@ -29,7 +29,6 @@ class ImportEventsTask
     needle.each do |n|
       count = count + haystack.scan(n).count
     end
-    puts count
     return count
   end
   
@@ -56,7 +55,7 @@ class ImportEventsTask
     # ******************************************************************
     
     # Affiche les événements à proximité de l'utilisateur
-    eventlocation = @graph.get_object("search?q=&type=place&center=" + latitude + "," + longitude + "&distance=40000&limit=40")
+    eventlocation = @graph.get_object("search?q=&type=place&center=" + latitude + "," + longitude + "&distance=40000&limit=400")
     @events_locations = eventlocation
     
     # **************************************************************
@@ -74,6 +73,7 @@ class ImportEventsTask
 
       # Insertion du lieu dans la base de données
       location.update_attributes(
+        :id_facebook => f["id"],
         :name  =>  f["name"],
         :city  =>  f["location"]["city"],
         :street  =>  f["location"]["street"],
@@ -114,6 +114,7 @@ class ImportEventsTask
         location = EventLocation.where(:id_facebook => event_location["id"]).first_or_initialize
 
         location.update_attributes(    
+          :id_facebook => event_location["id"]
           :name  =>  event_location["name"],
           :city  =>  event_location["location"]["city"],
           :street  =>  event_location["location"]["street"],
@@ -184,6 +185,7 @@ class ImportEventsTask
       puts "1 événement ajouté"
       
       event.update_attributes(
+        :id_facebook => event_request["id"],
         :title => event_request["name"],
         :picture => image_cover,
         :category => eventCategory,
@@ -195,63 +197,5 @@ class ImportEventsTask
       )
     end
   end
-    
-#   def self.fetch(token, latitude, longitude)
-#     # **************************************************************
-#     # Authentification process
-#     # **************************************************************
 
-#     # Authentification à l'application Facebook
-#     @oauth = Koala::Facebook::OAuth.new("653919454735658","035ddda1f5dc692934d9f0abc345e4ef", "/import/test")
-
-#     # Permission pour le graph search
-#     @oauth.url_for_oauth_code(:permissions => "publish_actions")
-
-#     # Autorisation pour le graph search
-#     @graph = Koala::Facebook::GraphAPI.new(token)
-
-#     # ******************************************************************
-#     # Get a list of bar using user GPS. I only get 40 bars at the moment
-#     # ******************************************************************
-
-#     # Affiche les événements à proximité de l'utilisateur
-#     eventlocation = @graph.get_object("search?q=bar&type=place&center=46.94,6.85&distance=40000&limit=40")
-#     @events_locations = eventlocation
-
-#     # **************************************************************
-#     # Get event list for each location and store them in eventlist
-#     # **************************************************************
-#     eventlist = Array.new
-#     eventlocation.each do |f| 
-#       eventlist_request = @graph.get_object(f["id"]+"/events?since=now")
-#       if !eventlist_request.empty?
-#         eventlist_request.each do |f| 
-#           eventlist.push(f)
-#         end
-#       end      
-#     end
-
-#     # **************************************************
-#     # Get events details and store them in @events
-#     # **************************************************
-#     @events = Array.new
-
-#     # Get events details loop
-#     eventlist.each do |fe| 
-#       puts "--------------------------------------------------------------------"
-#       puts fe
-#       puts "--------------------------------------------------------------------"
-
-#       event_request = @graph.get_object(fe["id"])
-#       @events = @events.push(event_request)
-#     end
-
-#     @events.each do |event|
-#       e = Event.new
-#       e.title = event["name"]
-#       e.event_location_id = 1
-#       e.save!
-#     end
-#   end
-  
 end
