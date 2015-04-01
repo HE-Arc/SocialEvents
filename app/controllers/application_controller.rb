10df2fa9bd35330a2985d26d51ef2dcbb15bda49
Mathieu Rosser <mathieu.rosser@he-arc.ch>
2015-04-01 20:11:05 +0000

    Add first login popup message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-    
-  before_filter :verify_current_user
+      
+  # special flash type for first login
+  add_flash_types :flash_first_login
   
+  before_filter :verify_current_user
+
   # filter to register information about the current logged user
   def verify_current_user
     user = current_user
