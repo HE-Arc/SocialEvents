ec13cb3b1c718028c5ebe027cafb714eb82f2f86
Mathieu Rosser <mathieu.rosser@he-arc.ch>
2015-04-04 15:41:41 +0000

    Handle recurring task job exceptions

--- config/initializers/tasks.rb
+++ config/initializers/tasks.rb
@@ -1,4 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
 # Remove old events
-DeleteEventsTask.schedule!
+begin
+  DeleteEventsTask.schedule!
+rescue
+  # if failing, the delayed jobs table couldn't be found... we can't do anything more!
+end
