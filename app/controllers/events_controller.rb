53468d9ba21e7f2530400bafc12faf7383e900d4
Mathieu Rosser <mathieu.rosser@he-arc.ch>
2015-04-04 15:23:36 +0000

    Correct GMaps for production

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,25 +1,5 @@
 class EventsController < ApplicationController
-  
-  helper_method :javascript_include_view_js
     
-  # Generate JS tags for GMaps API
-  def javascript_include_view_js
-    include = ''
-    
-    files = ["base.js", "base/base.js", "objects/base_builder.js", "objects/builder.js", "objects/handler.js", "objects/null_clusterer.js",
-            "google/objects/common.js", "google/builders/bound.js", "google/builders/circle.js", "google/builders/clusterer.js",
-            "google/builders/kml.js", "google/builders/map.js", "google/builders/marker.js", "google/builders/polygon.js",
-            "google/builders/polyline.js", "google/objects/bound.js", "google/objects/circle.js", "google/objects/clusterer.js",
-            "google/objects/kml.js", "google/objects/map.js", "google/objects/marker.js", "google/objects/polygon.js",
-            "google/objects/polyline.js", "google/primitives.js", "google.js"]
-    
-    files.each do |f|
-      include += '<script data-turbolinks-track="true" src="' + root_url + 'assets/gmaps/' + f + '?body=1" type="text/javascript"></script>'
-    end
-   
-    return include
-  end
-  
   # Show specified event
   def show 
     @event = Event.find(params[:id])
